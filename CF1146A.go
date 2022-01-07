9fc8d859eafaa40d164e66bef53c8fb97a93ef57
Pierre Etienne <etienne.pradere@gmail.com>
2022-01-07 06:41:44 -0500

    WIP

--- CF1146A.go
+++ CF1146A.go
@@ -1,55 +1,56 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "os"
-        "sort"
-        "strconv"
-        "strings"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
+
 type CF1146A struct {
-  sc        *bufio.Reader
-  split     []string
-  index     int
-  separator string
+	sc        *bufio.Reader
+	split     []string
+	index     int
+	separator string
 }
 
 func (in *CF1146A) GetLine() string {
-  line, err := in.sc.ReadString('\n')
-  if err != nil {
-  fmt.Println("error line:", line, " err: ", err)
-  }
-  in.split = []string{}
-  in.index = 0
-  return line
+	line, err := in.sc.ReadString('\n')
+	if err != nil {
+		fmt.Println("error line:", line, " err: ", err)
+	}
+	in.split = []string{}
+	in.index = 0
+	return line
 }
 func (in *CF1146A) load() {
-  if len(in.split) <= in.index {
-  in.split = strings.Split(in.GetLine(), in.separator)
-  in.index = 0
-  }
+	if len(in.split) <= in.index {
+		in.split = strings.Split(in.GetLine(), in.separator)
+		in.index = 0
+	}
 }
 
 func (in *CF1146A) NextInt() int {
-  in.load()
-  val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
-  in.index++
-  return val
+	in.load()
+	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
+	in.index++
+	return val
 }
 
 func (in *CF1146A) NextInt64() int64 {
-  in.load()
-  val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
-  in.index++
-  return val
+	in.load()
+	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
+	in.index++
+	return val
 }
 
 func (in *CF1146A) NextString() string {
-  in.load()
-  val := strings.TrimSpace(in.split[in.index])
-  in.index++
-  return val
+	in.load()
+	val := strings.TrimSpace(in.split[in.index])
+	in.index++
+	return val
 }
 
 /**
@@ -59,19 +60,33 @@ User: pepradere
 URL: https://codeforces.com/problemset/problem/1146/A
 Problem: CF1146A
 **/
-func (in *CF1146A) Run(){
-
+func (in *CF1146A) Run() {
+	str := in.NextString()
+	a := 0
+	for i := 0; i < len(str); i++ {
+		if str[i] == 'a' {
+			a++
+		}
+	}
+	mid := int(math.Ceil(float64(len(str)) / 2.0))
+	if a > mid {
+		fmt.Println(len(str))
+	} else if mid >= a {
+			fmt.Println(a + a - 1)
+	} else {
+		fmt.Println(a)
+	}
 }
 
 func NewCF1146A(r *bufio.Reader) *CF1146A {
-  return &CF1146A{
-  sc:        r,
-  split:     []string{},
-  index:     0,
-  separator: " ",
-  }
+	return &CF1146A{
+		sc:        r,
+		split:     []string{},
+		index:     0,
+		separator: " ",
+	}
 }
 
 func main() {
-  NewCF1146A(bufio.NewReader(os.Stdin)).Run()
+	NewCF1146A(bufio.NewReader(os.Stdin)).Run()
 }

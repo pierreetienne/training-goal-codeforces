6133de251f50a70a893a2e5c195646db7ffc114e
Pierre Etienne <etienne.pradere@gmail.com>
2022-01-22 07:41:17 -0500

    WIP

--- CF1624A.go
+++ CF1624A.go
@@ -8,14 +8,14 @@ import (
 	"strings"
 )
 
-type CF1626A struct {
+type CF1624A struct {
 	sc        *bufio.Reader
 	split     []string
 	index     int
 	separator string
 }
 
-func (in *CF1626A) GetLine() string {
+func (in *CF1624A) GetLine() string {
 	line, err := in.sc.ReadString('\n')
 	if err != nil {
 		fmt.Println("error line:", line, " err: ", err)
@@ -24,28 +24,28 @@ func (in *CF1626A) GetLine() string {
 	in.index = 0
 	return line
 }
-func (in *CF1626A) load() {
+func (in *CF1624A) load() {
 	if len(in.split) <= in.index {
 		in.split = strings.Split(in.GetLine(), in.separator)
 		in.index = 0
 	}
 }
 
-func (in *CF1626A) NextInt() int {
+func (in *CF1624A) NextInt() int {
 	in.load()
 	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
 	in.index++
 	return val
 }
 
-func (in *CF1626A) NextInt64() int64 {
+func (in *CF1624A) NextInt64() int64 {
 	in.load()
 	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
 	in.index++
 	return val
 }
 
-func (in *CF1626A) NextString() string {
+func (in *CF1624A) NextString() string {
 	in.load()
 	val := strings.TrimSpace(in.split[in.index])
 	in.index++
@@ -54,31 +54,38 @@ func (in *CF1626A) NextString() string {
 
 /**
 Run solve the problem CF1626A
-Date: 21/01/22
+Date: 12/01/22
 User: pepradere
-URL: https://codeforces.com/problemset/problem/1626/A
-Problem: CF1626A
+URL: https://codeforces.com/problemset/problem/1624/A
+Problem: CF1624A
 **/
-func (in *CF1626A) Run() {
+func (in *CF1624A) Run() {
 	for t := in.NextInt(); t > 0; t-- {
-		str := in.NextString()
-		mapa := make(map[uint8]int)
-		var sb strings.Builder
-		for i := 0; i < len(str); i++ {
-			val := mapa[str[i]]
-			mapa[str[i]] = val + 1
-		}
+		n := in.NextInt()
+		max := 0
+		min := 0
+		for i := 0; i < n; i++ {
+			num := in.NextInt()
+			if i == 0 {
+				min = num
+				max = num
+			} else {
+				if num > max {
+					max = num
+				}
 
-		for k, v := range mapa {
-			for i := 0; i < v; i++ {
-				sb.WriteString(string(k))
+				if num < min {
+					min = num
+				}
 			}
 		}
-		fmt.Println(sb.String())
+
+		fmt.Println(max - min)
+
 	}
 }
 
-func NewCF1626A(r *bufio.Reader) *CF1626A {
+func NewCF1624A(r *bufio.Reader) *CF1626A {
 	return &CF1626A{
 		sc:        r,
 		split:     []string{},

fddcd8889e6b9ab2cae632236718503a5c9f91e6
Pierre Etienne <etienne.pradere@gmail.com>
2022-07-12 20:16:31 -0500

    WIP

--- CF1696A.go
+++ CF1696A.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "os"
-        "sort"
-        "strconv"
-        "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
+
 type CF1696A struct {
-  sc        *bufio.Reader
-  split     []string
-  index     int
-  separator string
+	sc        *bufio.Reader
+	split     []string
+	index     int
+	separator string
 }
 
 func (in *CF1696A) GetLine() string {
-  line, err := in.sc.ReadString('\n')
-  if err != nil {
-  fmt.Println("error line:", line, " err: ", err)
-  }
-  in.split = []string{}
-  in.index = 0
-  return line
+	line, err := in.sc.ReadString('\n')
+	if err != nil {
+		fmt.Println("error line:", line, " err: ", err)
+	}
+	in.split = []string{}
+	in.index = 0
+	return line
 }
 func (in *CF1696A) load() {
-  if len(in.split) <= in.index {
-  in.split = strings.Split(in.GetLine(), in.separator)
-  in.index = 0
-  }
+	if len(in.split) <= in.index {
+		in.split = strings.Split(in.GetLine(), in.separator)
+		in.index = 0
+	}
 }
 
 func (in *CF1696A) NextInt() int {
-  in.load()
-  val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
-  in.index++
-  return val
+	in.load()
+	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
+	in.index++
+	return val
 }
 
 func (in *CF1696A) NextInt64() int64 {
-  in.load()
-  val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
-  in.index++
-  return val
+	in.load()
+	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
+	in.index++
+	return val
 }
 
 func (in *CF1696A) NextString() string {
-  in.load()
-  val := strings.TrimSpace(in.split[in.index])
-  in.index++
-  return val
+	in.load()
+	val := strings.TrimSpace(in.split[in.index])
+	in.index++
+	return val
 }
 
 /**
@@ -59,19 +59,27 @@ User: pepradere
 URL: https://codeforces.com/problemset/problem/1696/A
 Problem: CF1696A
 **/
-func (in *CF1696A) Run(){
-
+func (in *CF1696A) Run() {
+	for t := in.NextInt(); t > 0; t-- {
+		n, z := in.NextInt(), in.NextInt()
+		arr := make([]int, n)
+		max := 0
+		for i := 0; i < n; i++ {
+			arr[i] = in.NextInt()
+			arr[i] =
+		}
+	}
 }
 
 func NewCF1696A(r *bufio.Reader) *CF1696A {
-  return &CF1696A{
-  sc:        r,
-  split:     []string{},
-  index:     0,
-  separator: " ",
-  }
+	return &CF1696A{
+		sc:        r,
+		split:     []string{},
+		index:     0,
+		separator: " ",
+	}
 }
 
 func main() {
-  NewCF1696A(bufio.NewReader(os.Stdin)).Run()
+	NewCF1696A(bufio.NewReader(os.Stdin)).Run()
 }
--- CF931A.go
+++ CF931A.go
@@ -9,14 +9,14 @@ import (
 	"strings"
 )
 
-type CF1148A struct {
+type CF931A struct {
 	sc        *bufio.Reader
 	split     []string
 	index     int
 	separator string
 }
 
-func (in *CF1148A) GetLine() string {
+func (in *CF931A) GetLine() string {
 	line, err := in.sc.ReadString('\n')
 	if err != nil {
 		fmt.Println("error line:", line, " err: ", err)
@@ -25,28 +25,28 @@ func (in *CF1148A) GetLine() string {
 	in.index = 0
 	return line
 }
-func (in *CF1148A) load() {
+func (in *CF931A) load() {
 	if len(in.split) <= in.index {
 		in.split = strings.Split(in.GetLine(), in.separator)
 		in.index = 0
 	}
 }
 
-func (in *CF1148A) NextInt() int {
+func (in *CF931A) NextInt() int {
 	in.load()
 	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
 	in.index++
 	return val
 }
 
-func (in *CF1148A) NextInt64() int64 {
+func (in *CF931A) NextInt64() int64 {
 	in.load()
 	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
 	in.index++
 	return val
 }
 
-func (in *CF1148A) NextString() string {
+func (in *CF931A) NextString() string {
 	in.load()
 	val := strings.TrimSpace(in.split[in.index])
 	in.index++
@@ -54,29 +54,34 @@ func (in *CF1148A) NextString() string {
 }
 
 /**
-Run solve the problem CF1148A
-Date: 8/07/22
+Run solve the problem CF931A
+Date: 11/07/22
 User: pepradere
-URL: https://codeforces.com/problemset/problem/1148/A
-Problem: CF1148A
+URL: https://codeforces.com/problemset/problem/931/A
+Problem: CF931A
 **/
-func (in *CF1148A) Run() {
-	a, b, c := in.NextInt64(), in.NextInt64(), in.NextInt64()
+func (in *CF931A) Run() {
+	a := in.NextInt()
+	b := in.NextInt()
 
-	min := int64(math.Min(float64(a), float64(b)))
-	c += min
-	a -= min
-	b -= min
-	ans := c * 2
-	if a > 0 || b > 0 {
-		ans++
+	diff := int(math.Abs(float64(a)-float64(b)))
+	ans := 0
+
+	if diff % 2 == 0 {
+		div := diff/2
+		sum := (div*(div+1))/2
+		ans = 2*sum
+	}else {
+		div := int(math.Floor(float64(diff) / 2.))
+		ans = (div*(div+1))/2
+		ans += ((div+1)*(div+2))/2
 	}
-	fmt.Println(ans)
 
+	fmt.Println(ans)
 }
 
-func NewCF1148A(r *bufio.Reader) *CF1148A {
-	return &CF1148A{
+func NewCF931A(r *bufio.Reader) *CF931A {
+	return &CF931A{
 		sc:        r,
 		split:     []string{},
 		index:     0,
@@ -85,5 +90,5 @@ func NewCF1148A(r *bufio.Reader) *CF1148A {
 }
 
 func main() {
-	NewCF1148A(bufio.NewReader(os.Stdin)).Run()
+	NewCF931A(bufio.NewReader(os.Stdin)).Run()
 }

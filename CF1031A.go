1bd4b3ad3027c7a826578eb628c5d6ec65bdcbbb
Pi <etienne.pradere@gmail.com>
2022-08-13 11:11:15 -0500

    WIP

--- CF1031A.go
+++ CF1031A.go
@@ -8,14 +8,14 @@ import (
 	"strings"
 )
 
-type CF1696A struct {
+type CF1031A struct {
 	sc        *bufio.Reader
 	split     []string
 	index     int
 	separator string
 }
 
-func (in *CF1696A) GetLine() string {
+func (in *CF1031A) GetLine() string {
 	line, err := in.sc.ReadString('\n')
 	if err != nil {
 		fmt.Println("error line:", line, " err: ", err)
@@ -24,28 +24,28 @@ func (in *CF1696A) GetLine() string {
 	in.index = 0
 	return line
 }
-func (in *CF1696A) load() {
+func (in *CF1031A) load() {
 	if len(in.split) <= in.index {
 		in.split = strings.Split(in.GetLine(), in.separator)
 		in.index = 0
 	}
 }
 
-func (in *CF1696A) NextInt() int {
+func (in *CF1031A) NextInt() int {
 	in.load()
 	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
 	in.index++
 	return val
 }
 
-func (in *CF1696A) NextInt64() int64 {
+func (in *CF1031A) NextInt64() int64 {
 	in.load()
 	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
 	in.index++
 	return val
 }
 
-func (in *CF1696A) NextString() string {
+func (in *CF1031A) NextString() string {
 	in.load()
 	val := strings.TrimSpace(in.split[in.index])
 	in.index++
@@ -53,25 +53,32 @@ func (in *CF1696A) NextString() string {
 }
 
 /**
-Run solve the problem CF1696A
-Date: 3/07/22
-User: pepradere
-URL: https://codeforces.com/problemset/problem/1696/A
-Problem: CF1696A
+Run solve the problem CF1031A
+Date: 8/12/2022
+User: wotan
+URL: https://codeforces.com/problemset/problem/1031/A
+Problem: CF1031A
 **/
-func (in *CF1696A) Run() {
-	for t := in.NextInt(); t > 0; t-- {
-		n, _ := in.NextInt(), in.NextInt()
-		arr := make([]int, n)
+func (in *CF1031A) Run() {
+	w, h, k := in.NextInt(), in.NextInt(), in.NextInt()
 
-		for i := 0; i < n; i++ {
-			arr[i] = in.NextInt()
+	ans := 0
+	for k > 0 {
+
+		ans += (w + w + h + h) - 4
+		w -= 4
+		h -= 4
+		k--
+		if w <= 0 || h <= 0 {
+			break
 		}
 	}
+
+	fmt.Println(ans)
 }
 
-func NewCF1696A(r *bufio.Reader) *CF1696A {
-	return &CF1696A{
+func NewCF1031A(r *bufio.Reader) *CF1031A {
+	return &CF1031A{
 		sc:        r,
 		split:     []string{},
 		index:     0,
@@ -80,5 +87,5 @@ func NewCF1696A(r *bufio.Reader) *CF1696A {
 }
 
 func main() {
-	NewCF1696A(bufio.NewReader(os.Stdin)).Run()
+	NewCF1031A(bufio.NewReader(os.Stdin)).Run()
 }

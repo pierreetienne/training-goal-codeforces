8494490f124be15b9afd32a793e96c2b8505e08e
Pi <etienne.pradere@gmail.com>
2022-08-29 21:18:18 -0500

    WIP

--- CF1720A.go
+++ CF1720A.go
@@ -3,20 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type CF1679A struct {
+type CF1720A struct {
 	sc        *bufio.Reader
 	split     []string
 	index     int
 	separator string
 }
 
-func (in *CF1679A) GetLine() string {
+func (in *CF1720A) GetLine() string {
 	line, err := in.sc.ReadString('\n')
 	if err != nil {
 		fmt.Println("error line:", line, " err: ", err)
@@ -25,28 +24,28 @@ func (in *CF1679A) GetLine() string {
 	in.index = 0
 	return line
 }
-func (in *CF1679A) load() {
+func (in *CF1720A) load() {
 	if len(in.split) <= in.index {
 		in.split = strings.Split(in.GetLine(), in.separator)
 		in.index = 0
 	}
 }
 
-func (in *CF1679A) NextInt() int {
+func (in *CF1720A) NextInt() int {
 	in.load()
 	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
 	in.index++
 	return val
 }
 
-func (in *CF1679A) NextInt64() int64 {
+func (in *CF1720A) NextInt64() int64 {
 	in.load()
 	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
 	in.index++
 	return val
 }
 
-func (in *CF1679A) NextString() string {
+func (in *CF1720A) NextString() string {
 	in.load()
 	val := strings.TrimSpace(in.split[in.index])
 	in.index++
@@ -54,27 +53,29 @@ func (in *CF1679A) NextString() string {
 }
 
 /**
-Run solve the problem CF1679A
-Date: 8/21/2022
+Run solve the problem CF1720A
+Date: 8/27/2022
 User: wotan
-URL: https://codeforces.com/problemset/problem/1679/A
-Problem: CF1679A
+URL: https://codeforces.com/contest/1720/problem/A
+Problem: CF1720A
 **/
-func (in *CF1679A) Run() {
+func (in *CF1720A) Run() {
 	for t := in.NextInt(); t > 0; t-- {
-		n := in.NextInt64()
-		if n%2 != 0 && n < 4 {
-			fmt.Println(-1)
+		a, b, c, d := in.NextInt64(), in.NextInt64(), in.NextInt64(), in.NextInt64()
+		x := a * d
+		y := b * c
+		if x == y {
+			fmt.Println(0)
+		} else if y != 0 && x%y == 0 || x != 0 && y%x == 0 {
+			fmt.Println(1)
 		} else {
-			max := int64(math.Ceil(float64(n) / 4.))
-			min := int64(math.Ceil(float64(n) / 6.))
-			fmt.Println(min, max)
+			fmt.Println(2)
 		}
 	}
 }
 
-func NewCF1679A(r *bufio.Reader) *CF1679A {
-	return &CF1679A{
+func NewCF1720A(r *bufio.Reader) *CF1720A {
+	return &CF1720A{
 		sc:        r,
 		split:     []string{},
 		index:     0,
@@ -83,5 +84,5 @@ func NewCF1679A(r *bufio.Reader) *CF1679A {
 }
 
 func main() {
-	NewCF1679A(bufio.NewReader(os.Stdin)).Run()
+	NewCF1720A(bufio.NewReader(os.Stdin)).Run()
 }

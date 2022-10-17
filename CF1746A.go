7cd02b6dd98addd7e17198d2def9788abd97e0c5
Pi <etienne.pradere@gmail.com>
2022-10-17 13:10:13 -0500

    WIP

---
+++ CF1746A.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type CF1746A struct {
+	sc        *bufio.Reader
+	split     []string
+	index     int
+	separator string
+}
+
+func (in *CF1746A) GetLine() string {
+	line, err := in.sc.ReadString('\n')
+	if err != nil {
+		fmt.Println("error line:", line, " err: ", err)
+	}
+	in.split = []string{}
+	in.index = 0
+	return line
+}
+func (in *CF1746A) load() {
+	if len(in.split) <= in.index {
+		in.split = strings.Split(in.GetLine(), in.separator)
+		in.index = 0
+	}
+}
+
+func (in *CF1746A) NextInt() int {
+	in.load()
+	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
+	in.index++
+	return val
+}
+
+func (in *CF1746A) NextInt64() int64 {
+	in.load()
+	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
+	in.index++
+	return val
+}
+
+func (in *CF1746A) NextString() string {
+	in.load()
+	val := strings.TrimSpace(in.split[in.index])
+	in.index++
+	return val
+}
+
+/**
+Run solve the problem CF1746A
+Date: 10/16/2022
+User: wotan
+URL: https://codeforces.com/problemset/problem/1746/A
+Problem: CF1746A
+**/
+func (in *CF1746A) Run() {
+	for t := in.NextInt(); t > 0; t-- {
+		n, _ := in.NextInt(), in.NextInt()
+		ans := false
+		for i := 0; i < n; i++ {
+			val := in.NextInt()
+			if val == 1 {
+				ans = true
+			}
+		}
+		if ans {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+	}
+}
+
+func NewCF1746A(r *bufio.Reader) *CF1746A {
+	return &CF1746A{
+		sc:        r,
+		split:     []string{},
+		index:     0,
+		separator: " ",
+	}
+}
+
+func main() {
+	NewCF1746A(bufio.NewReader(os.Stdin)).Run()
+}

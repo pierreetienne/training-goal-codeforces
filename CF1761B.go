177fcdab9b7aaeed4149e64c0c798f725fefcce6
Pi <etienne.pradere@gmail.com>
2022-11-25 20:19:19 -0500

    WIP

--- CF1761B.go
+++ CF1761B.go
@@ -4,18 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-type CF1761A struct {
+type CF1761B struct {
 	sc        *bufio.Reader
 	split     []string
 	index     int
 	separator string
 }
 
-func (in *CF1761A) GetLine() string {
+func (in *CF1761B) GetLine() string {
 	line, err := in.sc.ReadString('\n')
 	if err != nil {
 		fmt.Println("error line:", line, " err: ", err)
@@ -24,28 +25,28 @@ func (in *CF1761A) GetLine() string {
 	in.index = 0
 	return line
 }
-func (in *CF1761A) load() {
+func (in *CF1761B) load() {
 	if len(in.split) <= in.index {
 		in.split = strings.Split(in.GetLine(), in.separator)
 		in.index = 0
 	}
 }
 
-func (in *CF1761A) NextInt() int {
+func (in *CF1761B) NextInt() int {
 	in.load()
 	val, _ := strconv.Atoi(strings.TrimSpace(in.split[in.index]))
 	in.index++
 	return val
 }
 
-func (in *CF1761A) NextInt64() int64 {
+func (in *CF1761B) NextInt64() int64 {
 	in.load()
 	val, _ := strconv.ParseInt(strings.TrimSpace(in.split[in.index]), 10, 64)
 	in.index++
 	return val
 }
 
-func (in *CF1761A) NextString() string {
+func (in *CF1761B) NextString() string {
 	in.load()
 	val := strings.TrimSpace(in.split[in.index])
 	in.index++
@@ -53,25 +54,18 @@ func (in *CF1761A) NextString() string {
 }
 
 /**
-Run solve the problem CF1761A
-Date: 11/23/2022
+Run solve the problem CF1761B
+Date: 11/24/2022
 User: wotan
-URL: https://codeforces.com/problemset/problem/1761/A
-Problem: CF1761A
+URL: https://codeforces.com/problemset/problem/1761/B
+Problem: CF1761B
 **/
-func (in *CF1761A) Run() {
-	for t := in.NextInt(); t > 0; t-- {
-		n, a, b := in.NextInt(), in.NextInt(), in.NextInt()
-		if n-a-b >= 2 || (n == a && a == b) {
-			fmt.Println("Yes")
-		} else {
-			fmt.Println("No")
-		}
-	}
+func (in *CF1761B) Run() {
+
 }
 
-func NewCF1761A(r *bufio.Reader) *CF1761A {
-	return &CF1761A{
+func NewCF1761B(r *bufio.Reader) *CF1761B {
+	return &CF1761B{
 		sc:        r,
 		split:     []string{},
 		index:     0,
@@ -80,5 +74,5 @@ func NewCF1761A(r *bufio.Reader) *CF1761A {
 }
 
 func main() {
-	NewCF1761A(bufio.NewReader(os.Stdin)).Run()
+	NewCF1761B(bufio.NewReader(os.Stdin)).Run()
 }
